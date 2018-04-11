19050bb262c75889197ac66518d44d063d4045ef
seyit <seyit@192.168.191.196>
2018-04-11 14:12:02 +0200

    Getter and Setter implemented
    -class Mitglieder

--- Vereinsverwaltung/src/com/sw/beans/Mitglieder.java
+++ Vereinsverwaltung/src/com/sw/beans/Mitglieder.java
@@ -9,12 +9,43 @@ public class Mitglieder
 		
 	}
 	
-	private int id;
 	private String name;
 	private String lname;
 	private Date birth;
 	private String team;
 	
 	// TODO Create Getter and Setter for attributes
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
 	
 }
