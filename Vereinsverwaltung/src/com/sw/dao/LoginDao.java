82b554307029f884de4976d358bd50dddc251ec7
tobi <tobi@tobias-air.fritz.box>
2018-04-12 17:13:04 +0200

    Version 1.1 TK - corrected LoginDao.java

--- Vereinsverwaltung/src/com/sw/dao/LoginDao.java
+++ Vereinsverwaltung/src/com/sw/dao/LoginDao.java
@@ -3,6 +3,8 @@ package com.sw.dao;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.sw.beans.User;
 
@@ -19,10 +21,29 @@ public class LoginDao
 				String sql = "Select * from roles where username like '%" + userToCheck.getUname() + "%' and pwd like '%"+ userToCheck.getPassword() + "%'";
 				Statement statement = connection.createStatement();
 				ResultSet set = statement.executeQuery(sql);
-				// Check if Username or Password is empty and if there is an existing entry
-				if(set.first() == true && (!userToCheck.getUname().isEmpty()) && (!userToCheck.getPassword().isEmpty()))
+				
+				List<User> userList = new ArrayList<User>();
+				
+				while(set.next())
 				{
-					return true;
+					User user = new User();
+					user.setPassword(set.getString("pwd"));
+					user.setUname(set.getString("username"));
+					
+					userList.add(user);
+				}
+				
+				// Check if Username or Password is empty and if there is an existing entry
+				if((!userToCheck.getUname().isEmpty()) && (!userToCheck.getPassword().isEmpty()))
+				{
+					for(int i = 0; i < userList.size(); i++)
+					{
+						if(userList.get(i).getPassword().equals(userToCheck.getPassword()) && userList.get(i).getUname().equals(userToCheck.getUname()))
+						{
+							return true;
+						}
+					}
+					
 				}
 			}
 			catch(Exception ex)
