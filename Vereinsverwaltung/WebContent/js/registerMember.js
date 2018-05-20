ac64c72ceb6d75a5358c201a1e8679763d450f25
tobi <tobi@tobias-air.fritz.box>
2018-05-20 15:56:33 +0200

    JavaScript Validate new Member

--- Vereinsverwaltung/WebContent/js/registerMember.js
+++ Vereinsverwaltung/WebContent/js/registerMember.js
@@ -1,108 +1,36 @@
-	  $( function() 
-		{
-	    var dialog, form,
-		  emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
-	      first_name = $( "#first_name" ),
-	      email_address = $( "#email_address" ),
-	      last_name = $( "#last_name" ),
-	      birth_date = $("#birth_date"),
-	      gender = $("#gender"),
-	      phone_number = $("#phone_number"),
-	      address_line = $("#address_line"),
-	      address_add = $("#address_add"),
-	      post_code = $("#post_code"),
-	      entry_date = $("#entry_date"),
-	      city = $("#city"),
-	      password = $("#password"),
-	      allFields = $( [] ).add( first_name ).add( last_name ).add( email_address ).add( birth_date).add( gender).add(phone_number).add(address_line).add(address_add).add(post_code).add(entry_date).add(city).add(password),
-	      tips = $( ".validateTips" );
-	 
-	    function updateTips( t ) {
-	      tips
-	        .text( t )
-	        .addClass( "ui-state-highlight" );
-	      setTimeout(function(){
-	        tips.removeClass( "ui-state-highlight", 1500 );
-	      }, 500 );
-	    }
-	    
-	    
-	 
-	    function checkLength( o, n, min, max ) {
-	      if ( o.val().length > max || o.val().length < min ) {
-	        o.addClass( "ui-state-error" );
-	        updateTips( "Length of " + n + " must be between " +
-	          min + " and " + max + "." );
-	        return false;
-	      } 
-	      else {
-	        return true;
-	      }
-	    }
-	 
-	    function checkRegexp( o, regexp, n ) {
-	      if ( !( regexp.test( o.val() ) ) ) {
-	        o.addClass( "ui-state-error" );
-	        updateTips( n );
-	        return false;
-	      } else {
-	        return true;
-	      }
-	    }
-	 
-	   function addUser() {
-	      var valid = true;
-	      allFields.removeClass( "ui-state-error" );
-		  if($("input[type='radio'].radioBtnClass").is(':checked')) {
-			  var gender_type = $("input[type='radio'].radioBtnClass:checked").val();
-		  }
-		  
-	     valid = valid && checkRegexp(email_address, emailRegex, "max.muster@hof-university.de");	
-	      if ( valid ) {
-	        $.post("registerMember", 
-	        		{first_name : first_name.val(), 
-	        		 email_address : email_address.val(),
-	        		 last_name : last_name.val(),
-	        		 birth_date : birth_date.val(),
-	        		 gender : gender_type,
-	        		 email_address : email_address.val(),
-	        		 phone_number : phone_number.val(),
-	        		 address_line : address_line.val(),
-	        		 address_add : address_add.val(),
-	        		 post_code : post_code.val(),
-	        		 entry_date : entry_date.val(),
-	        		 city : city.val(),
-	        		 });
-	        history.go(0);
-	        dialog.dialog( "close" );
-	      }
-	      return valid;
-	    }
-	 
-	    dialog = $( "#dialog-form" ).dialog({
-	      autoOpen: false,
-	      height: 700,
-	      width: 350,
-	      modal: true,
-	      buttons: {
-	        "Create an account": addUser,
-	        Cancel: function() {
-	          dialog.dialog( "close" );
-	        }
-	      },
-	      close: function() {
-	        form[ 0 ].reset();
-	        allFields.removeClass( "ui-state-error" );
-	      }
-	    });
-	 
-	    form = dialog.find( "form" ).on( "submit", function( event ) {
-	      event.preventDefault();
-	      history.go(0);
-	      addUser();
-	    });
-	 
-	    $( "#create-user" ).button().on( "click", function() {
-	      dialog.dialog( "open" );
-	    });
-	  } );
+function checkNewMember()
+{
+	var datereg = /^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$/;
+	
+	var birth_date = document.getElementById("birth_date_field").value;
+	var entry_date = document.getElementById("entry_date_field").value;
+	alert("nope");
+	if(!(datereg.test(birth_date)))
+	{
+		document.getElementById("birth_date_field").style.borderColor="#ff0000";
+		document.getElementById("birth_date_field").focus();
+		alert("nicht korrekt");
+		return false;
+	}
+	else
+	{
+		alert("korrekt");
+		document.getElementById("birth_date_field").style.borderColor="green";
+		document.getElementById("birth_date_field").focus();
+	}
+	
+	if(!(datereg.test(entry_date)))
+	{
+		document.getElementById("entry_date_field").style.borderColor="#ff0000";
+		document.getElementById("entry_date_field").focus();
+		alert("nicht korrekt");
+		return false;
+	}
+	else
+	{
+		document.getElementById("entry_date_field").style.borderColor="green";
+		document.getElementById("entry_date_field").focus();
+		alert("korrekt");
+		return true;
+	}
+}
